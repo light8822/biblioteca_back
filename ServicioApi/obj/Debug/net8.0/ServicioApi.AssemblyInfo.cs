ae82ce92fb1247b46be269b256ac371a09281954
Miguel Torres <mtorresh@utp.edu.pe>
2025-07-13 11:34:29 -0500

    Agregar .gitignore y eliminar archivos innecesarios del índice

--- ServicioApi/obj/Debug/net8.0/ServicioApi.AssemblyInfo.cs
+++ ServicioApi/obj/Debug/net8.0/ServicioApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServicioApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d21db514a7d807406de13d2cd97c794f9519c0a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServicioApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServicioApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
